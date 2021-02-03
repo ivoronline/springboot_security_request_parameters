00853f7609082858e973100b982779dce313d089
igoreta <ivoronline@gmail.com>
2021-02-03 09:16:03 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_request_parameters/SpringbootSecurityRequestParametersApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_request_parameters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityRequestParametersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSecurityRequestParametersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_security_request_parameters/SpringbootSecurityRequestParametersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_request_parameters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityRequestParametersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
